446a7ecf5fd17101c48b2bfb8666cf77bde68c13
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-13 14:18:38 +0300

    Assignment complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList ({posts}) {
+    const articlesItems = posts.map((post) => (
+        <Article 
+        key={post.id}
+        title={post.title}
+        date={post.date}
+        preview={post.preview}
+        minutes={post.minutes}
+        />
+    ))
+    return(
+        <main>
+            {articlesItems};
+        </main>
+    );
+}
+
+export default ArticleList;
